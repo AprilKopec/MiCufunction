c064a4469f66aebeb880fb0ab0b131ea83617cda
April Kopec <aprilkopec47@gmail.com>
2023-07-15 22:14:39 -0700

    Move cutscene, duration to own files

---
+++ duration.py
@@ -0,0 +1,16 @@
+from MiCufunction import Time
+
+class Duration:
+    takes_block = True
+    def __init__(self, stack, args) -> None:
+        self.parent = stack[-1]
+        self.duration = Time(args[1])
+
+    def begin(self) -> list[str]:
+        return []
+
+    def prefix(self) -> str:
+        return f'execute if score t {self.parent.objective.name} matches {self.parent.time.ticks}..{self.parent.time+self.duration} run tellraw @a "{line[4:]}"'
+
+    def end(self)-> list[str]:
+        return []
