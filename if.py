a2312a2bd0be1619801ad097a8b7f0ac155448f3
April Kopec <aprilkopec47@gmail.com>
2023-07-16 00:55:26 -0700

    start implementing if

--- if.py
+++ if.py
@@ -1,21 +1,22 @@
 from utils import Time, Objective
 
-class Cutscene:
+class If:
     takes_block = True
     def __init__(self, stack, line, args) -> None:
         self.time = Time(1)
         self.latest_time = Time(1)
         self.objective = Objective(args[1])
         self.text = self.begin()
-        
-        self.timer_number = 0
-        self.timer_name = "t"
+        self.parent = stack[-1]
+
+        self.timer_number = self.parent.timer_number + 1
+        self.timer_name = "t" + self.timer_number
 
     def begin(self) -> list[str]:
         return [
           "### Cutscene setup ###",
           f"scoreboard objectives add {self.objective.name} dummy",
-          f"scoreboard players add {'t' + self.timer_number} {self.objective.name} 1",
+          f"scoreboard players add t {self.objective.name} 1",
           f"scoreboard players set endCutscene {self.objective.name} 0",
           "",
           "### Cutscene ###"
@@ -26,12 +27,12 @@ class Cutscene:
         return [
           "",
           "### Cutscene Cleanup ###",
-          f'execute if score {self.timer_name} {self.objective.name} matches {self.latest_time} run scoreboard players set endCutscene {self.objective.name} 1',
-          f'execute if score endCutscene {self.objective.name} matches 1 run scoreboard players set {self.timer_name} {self.objective.name} 0',
+          f'execute if score t {self.objective.name} matches {self.latest_time} run scoreboard players set endCutscene {self.objective.name} 1',
+          f'execute if score endCutscene {self.objective.name} matches 1 run scoreboard players set t {self.objective.name} 0',
           "",
           "### Run cutscene every tick ###",
           f"execute unless score endCutscene {self.objective.name} matches 1 run schedule function OUTNAME 1t append"
         ]
 
     def prefix(self) -> str:
-        return f"execute if score {self.timer_name} {self.objective.name} matches {self.time} run"
+        return f"execute if score t {self.objective.name} matches {self.time} run"
