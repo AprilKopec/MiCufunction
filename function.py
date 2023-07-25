becfdf5b3525ef2cc9a28550e5f885c01a0e9bc3
April Kopec <aprilkopec47@gmail.com>
2023-07-24 19:16:21 -0700

    make cutscenes not run multiple times per tick

--- function.py
+++ function.py
@@ -6,7 +6,11 @@ class Function(Control_Flow):
     def __init__(self, stack, line, args) -> None:
         self.function_name = args[1]
         self.camera = Camera_Position(0, 0, 0)
+
+        # These are only needed for cutscenes, if we implement functions that aren't cutscenes
         self.forcequit = "force_quit"
+        self.global_timer = "global_timer"
+
         super().__init__(stack, line, args)
 
     def get_objective(self) -> Objective:
@@ -31,7 +35,15 @@ class Function(Control_Flow):
           f'execute if score {self.end_name} {self.objective.name} matches 1 run scoreboard players set {self.timer_name} {self.objective.name} 0',
           "",
           "### Run cutscene every tick ###",
-          f"execute unless score {self.end_name} {self.objective.name} matches 1 unless score {self.forcequit} {self.objective.name} matches 1 run schedule function {self.function_name} 1t append"
+          # Check that the cutscene isn't running multiple times per tick
+          f"scoreboard players operation {self.global_timer} {self.objective.name} -= t Tt2GlobalTimer",
+          f"execute if score {self.global_timer} {self.objective.name} matches 0 run scoreboard players set {self.forcequit} {self.objective.name} 1",
+          f"scoreboard players operation {self.global_timer} {self.objective.name} = t Tt2GlobalTimer",
+          # Schedule function if conditions met
+          f"execute unless score {self.end_name} {self.objective.name} matches 1 unless score {self.forcequit} {self.objective.name} matches 1 run schedule function {self.function_name} 1t append",
+          # If we just leave this set to 1 then it might softlock the player I think
+          # Maybe I could copy it to some other scoreboard value first for debug if that seems necessary
+          f"scoreboard players set {self.forcequit} {self.objective.name} 0"
         ]
 
     def give_prefix(self) -> str:
