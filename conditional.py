14082e3789e0db3bbf45985e783b84a3a5b90077
April Kopec <aprilkopec47@gmail.com>
2023-07-27 21:53:45 -0700

    some commenting and if block refactoring

--- conditional.py
+++ conditional.py
@@ -7,19 +7,27 @@ class If(Control_Flow):
     def __init__(self, stack, line, args) -> None:
         self.parent = stack[-1]
         self.condition = " ".join(args[1:-1])
-        super().__init__(stack, line, args)
-
-        self.condition_name = self.add_number("condition", self.depth)
         self.condition_value = "1" # We will change this to 0 for else
-
+        super().__init__(stack, line, args)
+        self.condition_name = self.add_number("condition", self.depth)
+        self.condition_checked = self.add_number("condition_checked", self.depth)
+        self.execute_if_condition = f'execute if score {self.condition_name} {self.objective} matches {self.condition_value}'
+    
     def begin(self) -> list[str]:
         # This is a little hacky but it makes the camera slightly less incompatible with conditionals
         self.camera = copy(self.parent.camera)
         text = [
-          f"{self.condition} run scoreboard players set {self.condition_name} {self.condition_value}"
-          f"execute if score {self.condition_name} matches {self.condition_value} run scoreboard players set {self.parent.pause_name} {self.parent.objective.name} 1",
-          f"execute if score {self.condition_name} matches {self.condition_value} unless score {self.pause_name} {self.objective.name} matches 1 run scoreboard players add {self.timer_name} {self.objective.name} 1",
-          f"execute if score {self.condition_name} matches {self.condition_value} run scoreboard players set {self.end_name} {self.objective.name} 0"
+          # Check condition, store result
+          f"execute if {self.condition} unless score {self.condition_checked} {self.objective} matches 1 run scoreboard players set {self.condition_name} 1",
+          f"execute unless score {self.condition_name} {self.objective} matches 1 unless score {self.condition_checked} {self.objective} matches 1 run scoreboard players set {self.condition_name} 0",
+          # We only want to check condition once
+          f"scoreboard players set {self.condition_checked} {self.objective} 1",
+          # If condition met, pause parent timer
+          f"{self.execute_if_condition} run scoreboard players set {self.parent.pause_name} {self.parent.objective.name} 1",
+          # If condition met and not paused by a further if block, increment timer
+          f"{self.execute_if_condition} unless score {self.pause_name} {self.objective} matches 1 run scoreboard players add {self.timer_name} {self.objective} 1",
+          # Make sure function can replay correctly
+          f"{self.execute_if_condition} run scoreboard players set {self.end_name} {self.objective} 0"
         ]
         return ["    " + self.add_prefix(line) for line in text] + [""]
 
@@ -28,25 +36,26 @@ class If(Control_Flow):
             self.parent.time += Time(1) # We want the cutscene to wait until the tick after the if block executes
         self.latest_time = max(self.latest_time, self.time)
         text = [
-          f'execute if score {self.condition_name} matches {self.condition_value} if score {self.timer_name} {self.objective.name} matches {self.latest_time} run scoreboard players set {self.end_name} {self.objective.name} 1',
-          f'execute if score {self.condition_name} matches {self.condition_value} if score {self.end_name} {self.objective.name} matches 1 run scoreboard players set {self.timer_name} {self.objective.name} 0',
-          f'execute if score {self.condition_name} matches {self.condition_value} if score {self.end_name} {self.objective.name} matches 1 run scoreboard players set {self.parent.pause_name} {self.parent.objective.name} 0'
+          f'{self.execute_if_condition} if score {self.timer_name} {self.objective} matches {self.latest_time} run scoreboard players set {self.end_name} {self.objective} 1',
+          f'{self.execute_if_condition} if score {self.end_name} {self.objective} matches 1 run scoreboard players set {self.timer_name} {self.objective} 0',
+          f'{self.execute_if_condition} if score {self.end_name} {self.objective} matches 1 run scoreboard players set {self.parent.pause_name} {self.parent.objective.name} 0'
         ]
         return [""] + ["    " + self.add_prefix(line) for line in text]
 
     def give_prefix(self) -> str:
-        return "    " + self.add_prefix(f"execute if score {self.condition_name} matches {self.condition_value} if score {self.timer_name} {self.objective.name} matches {self.time} run")
+        return "    " + self.add_prefix(f"{self.execute_if_condition} if score {self.timer_name} {self.objective} matches {self.time} run")
     
+# Probably if and else should both descend from a superclass but whatever
 class Else(If):
     takes_block = True
     def __init__(self, if_block) -> None:
         assert isinstance(if_block, If), "else must follow if"
         self.parent = if_block.parent
-        self.condition = " ".join(if_block.args[1:-1])
-        Control_Flow.__init__(self, if_block.stack, if_block.line, if_block.args)
-
-        self.condition_name = self.add_number("condition", self.depth)
+        self.condition = if_block.condition # Unused, but crashes when you call super().begin() if not set. Maybe refactor?
+        self.condition_name = if_block.condition_name
         self.condition_value = "0"
+        self.execute_if_condition = f'execute if score {self.condition_name} {if_block.objective} matches {self.condition_value}'
+        Control_Flow.__init__(self, if_block.stack, if_block.line, if_block.args)
 
     def begin(self) -> list[str]:
         return super().begin()[1:] # We don't want to check the condition again
--- function.py
+++ function.py
@@ -19,9 +19,12 @@ class Function(Control_Flow):
     def begin(self) -> list[str]:
         return [
           "### Cutscene setup ###",
-          f"scoreboard objectives add {self.objective.name} dummy",
-          f"execute unless score {self.pause_name} {self.objective.name} matches 1 run scoreboard players add {self.timer_name} {self.objective.name} 1",
-          f"scoreboard players set {self.end_name} {self.objective.name} 0",
+          # Make sure the scoreboard objective exists
+          f"scoreboard objectives add {self.objective} dummy",
+          # Increment timer unless part of the function has made a separate timer
+          f"execute unless score {self.pause_name} {self.objective} matches 1 run scoreboard players add {self.timer_name} {self.objective} 1",
+          # Make sure function doesn't end early and can replay
+          f"scoreboard players set {self.end_name} {self.objective} 0",
           "",
           "### Cutscene ###"
         ]
@@ -31,20 +34,20 @@ class Function(Control_Flow):
         return [
           "",
           "### Cutscene Cleanup ###",
-          f'execute if score {self.timer_name} {self.objective.name} matches {self.latest_time} run scoreboard players set {self.end_name} {self.objective.name} 1',
-          f'execute if score {self.end_name} {self.objective.name} matches 1 run scoreboard players set {self.timer_name} {self.objective.name} 0',
+          # End the function after the last event ends
+          f'execute if score {self.timer_name} {self.objective} matches {self.latest_time} run scoreboard players set {self.end_name} {self.objective} 1',
+          # Reset the timer for next time function is used
+          f'execute if score {self.end_name} {self.objective} matches 1 run scoreboard players set {self.timer_name} {self.objective} 0',
           "",
           "### Run cutscene every tick ###",
-          # Check that the cutscene isn't running multiple times per tick
-          f"scoreboard players operation {self.global_timer} {self.objective.name} -= t Tt2GlobalTimer",
-          f"execute if score {self.global_timer} {self.objective.name} matches 0 run scoreboard players set {self.forcequit} {self.objective.name} 1",
-          f"scoreboard players operation {self.global_timer} {self.objective.name} = t Tt2GlobalTimer",
-          # Schedule function if conditions met
-          f"execute unless score {self.end_name} {self.objective.name} matches 1 unless score {self.forcequit} {self.objective.name} matches 1 run schedule function {self.function_name} 1t append",
-          # If we just leave this set to 1 then it might softlock the player I think
-          # Maybe I could copy it to some other scoreboard value first for debug if that seems necessary
-          f"scoreboard players set {self.forcequit} {self.objective.name} 0"
+          # Check that global timer has incremented since the last time the function was ran; force-quit the cutscene if it's run multiple times in the same tick
+          f"scoreboard players operation global_timer {self.objective} -= t Tt2GlobalTimer",
+          f"execute if score global_timer {self.objective} matches 0 run scoreboard players set {self.forcequit} {self.objective} 1",
+          f"scoreboard players operation global_timer {self.objective} = t Tt2GlobalTimer",
+          # Schedule function if not over and not forcequit
+          f"execute unless score {self.end_name} {self.objective} matches 1 unless score {self.forcequit} {self.objective} matches 1 run schedule function {self.function_name} 1t append",
+          # Maybe we want to set this up so that if the cutscene runs multiple times it only resets the extras? Might be more resilient to glitches then
         ]
 
     def give_prefix(self) -> str:
-        return f"execute if score {self.timer_name} {self.objective.name} matches {self.time} run"
+        return f"execute if score {self.timer_name} {self.objective} matches {self.time} run"
