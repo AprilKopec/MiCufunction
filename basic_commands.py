879c22c6ec1f5b9b761cdb6466b988cdcd3e1400
April Kopec <aprilkopec47@gmail.com>
2023-08-18 13:57:20 -0700

    i think this works

--- basic_commands.py
+++ basic_commands.py
@@ -2,8 +2,8 @@ from utils import Time
 from command_baseclass import MiCufunction_Command
 from typing import List
 
-def rest(string):
-    return string.split(" ", 1)[1]
+def rest(string, n: int = 1) -> str:
+    return string.split(" ", n)[n]
 
 class Basic_Command(MiCufunction_Command):
     def __init__(self, stack, line: str, args: List[str]):
@@ -30,7 +30,35 @@ class Command(Basic_Command):
 class Wait(Basic_Command):
     def pretext(self):
         self.timekeeper = self.find_timekeeper(self.stack)
-        self.timekeeper.time += Time(self.args[1])
+        if(self.args[1] not in ["until", "while"]):
+            try:
+                self.timekeeper.time += Time(self.args[1])
+            except:
+                raise ValueError("Valid arguments for 'wait' are 'until', 'while', or a duration.")
+
+    def get_text(self):
+        if(self.args[1] in ["until", "while"]):
+            timer_name = self.timekeeper.timer_name
+            pause_name = self.timekeeper.pause_name
+            objective = self.timekeeper.objective
+            b = 1 if self.args[1] == "until" else 0
+
+            text = [self.add_prefix(f'scoreboard players add {timer_name} {objective} 1')]
+            self.timekeeper.time += Time(1)
+
+            text1 = [
+                f'scoreboard players set {pause_name} {objective} 1',
+                f'scoreboard players set temp {objective} 0'
+                f'execute if {rest(self.line,2)} run scoreboard players set temp {objective} 1'
+                f'execute if score temp {objective} matches {b} run scoreboard players set {pause_name} {objective} 0',
+                f'execute if score temp {objective} matches {b} run scoreboard players add {timer_name} {objective} 1'
+            ]
+            text += [self.add_prefix(line) for line in text1]
+            self.timekeeper.time += Time(1)
+            return text
+
+        else:
+            return []
 
 class Comment(Basic_Command):
     def get_text(self):
